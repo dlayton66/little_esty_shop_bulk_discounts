95530e74391d4da6b9583be424523b87bef03a13
Drew Layton <dlayton66@gmail.com>
2023-01-14 09:21:12 -0700

    Add merchant fk to bulk_discounts

--- db/migrate/20230114162014_create_bulk_discounts.rb
+++ db/migrate/20230114162014_create_bulk_discounts.rb
@@ -3,6 +3,7 @@ class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
     create_table :bulk_discounts do |t|
       t.integer :discount
       t.integer :quantity
+      t.references :merchant, foreign_key: true
 
       t.timestamps
     end
