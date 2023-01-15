6d5b986212bf7310fb90354c9d1ee0de699907fe
Drew Layton <dlayton66@gmail.com>
2023-01-14 17:11:28 -0700

    feat/test: US1: Add links and discount attributes to discounts index

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,4 +2,8 @@ class BulkDiscountsController < ApplicationController
   def index
     @bulk_discounts = BulkDiscount.where(merchant_id: params[:merchant_id])
   end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
 end
--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'bulk discounts index' do
-  it 'lists all discounts' do
-    merchant_1 = Merchant.create!(name: 'Hair Care')
-    merchant_2 = Merchant.create!(name: 'Jewelry')
+  before :each do
+    @merchant_1 = Merchant.create!(name: 'Hair Care')
+    @merchant_2 = Merchant.create!(name: 'Jewelry')
+  
+    @bulk_discounts = FactoryBot.create_list(:bulk_discount, 3, merchant: @merchant_1)
+    @bulk_discount = FactoryBot.create(:bulk_discount, merchant: @merchant_2)
+  
+    visit merchant_bulk_discounts_path(@merchant_1)
+  end
+  
+  it 'lists all discounts with links to show page' do
+    expect(page).to have_link(@bulk_discounts[0].name, href: merchant_bulk_discount_path(@merchant_1, @bulk_discounts[0]))
+    expect(page).to have_link(@bulk_discounts[1].name, href: merchant_bulk_discount_path(@merchant_1, @bulk_discounts[1]))
+    expect(page).to have_link(@bulk_discounts[2].name, href: merchant_bulk_discount_path(@merchant_1, @bulk_discounts[2]))
+  
+    expect(page).to_not have_content(@bulk_discount.name)
+  end
 
-    bulk_discounts = FactoryBot.create_list(:bulk_discount, 3, merchant: merchant_1)
-    bulk_discount = FactoryBot.create(:bulk_discount, merchant: merchant_2)
-
-    visit merchant_bulk_discounts_path(merchant_1)
-
-    expect(page).to have_content(bulk_discounts[0].name)
-    expect(page).to have_content(bulk_discounts[1].name)
-    expect(page).to have_content(bulk_discounts[2].name)
-
-    expect(page).to_not have_content(bulk_discount.name)
+  it 'lists discount info' do
+    within("#bulk_discount-#{@bulk_discounts[0].id}") do
+      expect(page).to have_content(@bulk_discounts[0].discount)
+      expect(page).to have_content(@bulk_discounts[0].quantity)
+    end
   end
 end
