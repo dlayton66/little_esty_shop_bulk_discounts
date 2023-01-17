125eb9abf0f12cceec77392bece7ca469053cdb1
Drew Layton <dlayton66@gmail.com>
2023-01-16 19:14:52 -0700

    refactor: Add top_discount helper method in InvoiceItem

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -44,36 +44,48 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe 'instance methods' do
+    before :each do
+      @m1 = Merchant.create!(name: 'Merchant 1')
+  
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+  
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+  
+      @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @m1.id)
+      @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @m1.id)
+      @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @m1.id)  
+      @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @m1.id)
+      @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @m1.id)
+  
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 5, unit_price: 10, status: 0)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 10, unit_price: 8, status: 0)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_3.id, quantity: 15, unit_price: 5, status: 2)
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_4.id, quantity: 20, unit_price: 5, status: 1)
+      @ii_5 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_5.id, quantity: 1, unit_price: 5, status: 1)
+  
+      @bulk_discount_1 = BulkDiscount.create!(name: "Christmas", discount: 30, quantity: 20, merchant_id: @m1.id)
+      @bulk_discount_2 = BulkDiscount.create!(name: "Easter", discount: 20, quantity: 19, merchant_id: @m1.id)
+      @bulk_discount_3 = BulkDiscount.create!(name: "MLK", discount: 15, quantity: 8, merchant_id: @m1.id)
+      @bulk_discount_4 = BulkDiscount.create!(name: "Boxing Day", discount: 5, quantity: 5, merchant_id: @m1.id)
+    end
+
     describe '.discounted_revenue' do
       it 'returns the discounted revenue for the invoice item' do
-        @m1 = Merchant.create!(name: 'Merchant 1')
-
-        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-
-        @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-
-        @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @m1.id)
-        @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @m1.id)
-        @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @m1.id)  
-        @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @m1.id)
-        @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @m1.id)
-
-        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 5, unit_price: 10, status: 0)
-        @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 10, unit_price: 8, status: 0)
-        @ii_3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_3.id, quantity: 15, unit_price: 5, status: 2)
-        @ii_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_4.id, quantity: 20, unit_price: 5, status: 1)
-        @ii_5 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_5.id, quantity: 1, unit_price: 5, status: 1)
-  
-        @bulk_discount_1 = BulkDiscount.create!(name: "Christmas", discount: 30, quantity: 20, merchant_id: @m1.id)
-        @bulk_discount_2 = BulkDiscount.create!(name: "Easter", discount: 20, quantity: 19, merchant_id: @m1.id)
-        @bulk_discount_3 = BulkDiscount.create!(name: "MLK", discount: 15, quantity: 8, merchant_id: @m1.id)
-        @bulk_discount_4 = BulkDiscount.create!(name: "Boxing Day", discount: 5, quantity: 5, merchant_id: @m1.id)
-
         expect(@ii_1.discounted_revenue).to eq(47.5)
         expect(@ii_2.discounted_revenue).to eq(68)
         expect(@ii_3.discounted_revenue).to eq(63.75)
         expect(@ii_4.discounted_revenue).to eq(70)
         expect(@ii_5.discounted_revenue).to eq(5)
+      end
+    end
+
+    describe '.top_discount' do
+      it 'returns the best discount for the invoice item' do
+        expect(@ii_1.top_discount).to eq(@bulk_discount_4)
+        expect(@ii_2.top_discount).to eq(@bulk_discount_3)
+        expect(@ii_3.top_discount).to eq(@bulk_discount_3)
+        expect(@ii_4.top_discount).to eq(@bulk_discount_1)
+        expect(@ii_5.top_discount).to eq([])
       end
     end
   end
